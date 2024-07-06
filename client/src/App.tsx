8e70d32dc8837348bb3df3356b36296846ba7a65
Arth <probablyarth@gmail.com>
2024-07-07 03:57:04 +0530

    client init

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>This is a button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
